025d73e85894aa58f9ec2f9cd1d239ce53a2f77e
Aeronami <93769415+aeronami09@users.noreply.github.com>
2026-02-10 15:07:35 +0800

    week2.py

---
+++ week2.py
@@ -0,0 +1,16 @@
+fullname = "Aaron Nathaniel"
+age = 17
+height = 1.85
+currently_working = False
+
+print("Fullname:", fullname)
+print(type(fullname))
+
+print("Age:", age)
+print(type(age))
+
+print("Height in meters:", height)
+print(type(height))
+
+print("Currently working?", currently_working)
+print(type(currently_working))
